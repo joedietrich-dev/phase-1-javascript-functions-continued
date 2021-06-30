625f19ce86d9c4179423b4f645d6062cccb92682
Joe Dietrich <joe@joedietrich.net>
2021-06-29 23:09:29 -0400

    commit tree d07dcf734c548ec3df847c774be7f58e2744746a

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function mw(activity = 'go to the office') {
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair = '*') {
+  return function subjectInput(subject) {
+    return `You are ${flair}${subject}${flair}!`;
+  }
+}
